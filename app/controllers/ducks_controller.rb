47aa9262b02722e9365335b88e9734ba6cb900b2
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-06-13 01:34:23 -0400

    functioning CRUD rails app

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -4,4 +4,41 @@ class DucksController < ApplicationController
     @ducks = Duck.all
   end
 
+  def show
+    @duck = Duck.find(params[:id])
+    @owner = @duck.student
+  end
+
+  def new
+    @duck = Duck.new
+    @students = Student.all
+  end
+
+  def create
+    @duck = Duck.create(duck_params)
+    redirect_to @duck
+  end
+
+  def edit
+    @duck = Duck.find(params[:id])
+  end
+
+  def update
+    @duck = Duck.find(params[:id])
+    @duck.update(duck_params)
+    redirect_to @duck
+  end
+
+  def destroy
+    @duck = Duck.find(params[:id])
+    @duck.destroy
+    redirect_to ducks_path
+  end
+
+  private
+
+  def duck_params
+    params.require(:duck).permit(:name, :description, :student_id)
+  end
+
 end

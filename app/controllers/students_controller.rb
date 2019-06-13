3fc0f7e0a28d93382d20e685dae9a86606b524b8
DDuongNguyen <duong.nguyen4991@gmail.com>
2019-06-13 17:33:59 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,43 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+    @ducks = @student.ducks
+  end
+
+  def new
+    @student = Student.new()
+  end
+
+  def create
+    student = Student.create(student_params)
+    redirect_to student
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+    # student.update(student_params)
+    # redirect_to @student
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_params)
+    if @student.valid?
+      redirect_to @student
+    else
+    flash[:errors] = @student.errors.full_messages
+      redirect_to edit_student_path(@student)
+    end
+  end
+
+private
+def student_params
+  params.require(:student).permit(:name, :mod)
+end
+
+# def find_student
+#   @student = Student.find(params[:id])
+# end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,6 @@
 class Student < ApplicationRecord
+  @@num = 5
+  has_many :ducks
+  validates :name, presence: true, uniqueness: true
+  validates :mod, presence: true, inclusion: { in:1..@@num, message: "Number can only be between 1..#{@@num}" },numericality: { only_integer: true }
 end

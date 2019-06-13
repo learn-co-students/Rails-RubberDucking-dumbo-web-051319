71680da8d15507ae377f7e5428cfeaeb9484b415
tommaclean <tom.j.maclean@gmail.com>
2019-06-13 17:48:18 -0400

    Done.

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,7 +1,52 @@
 class DucksController < ApplicationController
+  before_action :find_duck, only: [:show, :edit, :update, :delete]
+
 
   def index
     @ducks = Duck.all
   end
 
+  def new
+    @duck = Duck.new
+    @students = Student.all
+  end
+
+  def create
+    @duck = Duck.create(duck_params)
+    if @duck.valid?
+      redirect_to duck_path(@duck)
+    else
+      flash[:errors] = @duck.errors.full_messages
+      redirect_to new_duck_path
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+    @ducks = Duck.all
+  end
+
+  def update
+    @duck.update(duck_params)
+    redirect_to duck_path
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_duck
+    @duck = Duck.find(params[:id])
+  end
+
+  def duck_params
+    params.require(:duck).permit(:name, :description, :student_id)
+  end
 end
---
+++ test/models/student_duck_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StudentDuckTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

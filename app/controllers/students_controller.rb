ecf0c677a551edb225153a5210867d18819e222a
sticklercarla <carlastickler@gmail.com>
2019-06-12 22:43:24 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,36 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+    if @student.valid?
+      redirect_to @student
+    else
+      flash[:errors] = @student.errors.full_messages
+      redirect_to new_student_path
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :mod)
+  end
+
 end

afd3b9653d3cec6fb50af6ad60257e797026a992
SimplyAvi <a.totaram@gmail.com>
2019-06-15 17:20:14 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,18 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update
+    redirect_to Student_path(@student)
+  end
+
 end
